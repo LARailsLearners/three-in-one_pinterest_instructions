c273de4fe044940e04f43a97db1cbeccf3032abd
jendiamond <thejendiamond@gmail.com>
2015-08-24 22:39:16 -0700

    Add Devise and User association

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ PhoebesPinterest::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Needed for Devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
